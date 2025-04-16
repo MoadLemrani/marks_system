1388854d93166a8bf8cd37f7b0354215d417869d
moad <moad@moad>
2025-04-16 11:43:37 +0100

    t9ad lformulaire, page etudiant, page prof

---
+++ login.php
@@ -0,0 +1,47 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "marks_system_db");
+if (!$conn) {
+    die("Connection failed : " . mysqli_connect_error());
+}
+
+
+
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {//REMARQUE: 9bl ma dir hadxi khask checki wax luser 3mr lformulaire
+
+
+    $email = $_POST['email'] ?? null;//b "?? null" ila luser khla lfield khawi PHP maghadix ysif undefiened warnins ms ay3amlo ka null
+    $password = $_POST['password'] ?? null;
+
+    if ($email && $password) {//taydhl luser lfields bjoj 3Ad khdem
+
+        //check wax lcompte kayn w dyal axmn type d l user 
+
+        //using prepared statemen to avoid sql injection
+        $sqletudiant = "SELECT * FROM etudiant WHERE email = ? AND pwd = ?";
+        $stmt = mysqli_prepare($conn, $sqletudiant);//prepare
+        mysqli_stmt_bind_param($stmt, "ss", $email, $password);//replace the placeholders"?"
+        mysqli_stmt_execute($stmt);//execute
+        $resultetudiant = mysqli_stmt_get_result($stmt);//getting result
+
+        if (mysqli_num_rows($resultetudiant) == 1) {//used "== 1" instead of "> 0" cuz the email is unique so it cant exist in more than 1 row
+            header("Location: studentpage.html");//la kan luser etudiant atsifto lpage lmkhssa lihom
+            exit();//this stops the script
+        } else {
+            $sqlprof = "SELECT * FROM prof WHERE email = ? AND pwd = ?";
+            $stmt = mysqli_prepare($conn, $sqlprof);
+            mysqli_stmt_bind_param($stmt, "ss", $email, $password);
+            mysqli_stmt_execute($stmt);
+            $resultprof = mysqli_stmt_get_result($stmt);
+            if (mysqli_num_rows($resultprof) == 1) {
+                header("Location: profpage.html");
+                exit();
+            } else {
+                echo "E-mail or password incorrect, try again pls";
+            }
+        }
+    } else {
+        echo "Please enter both E-mail and password";
+    }
+}
+?>
